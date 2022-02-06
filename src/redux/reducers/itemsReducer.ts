a464983ea8ce605a1901404e52a05bf415f1fbea
ZorikovPasha <zorikov.pasha@mail.ru>
2022-02-06 22:13:56 +0300

    сортировка

--- src/redux/reducers/itemsReducer.ts
+++ src/redux/reducers/itemsReducer.ts
@@ -1,20 +1,40 @@
-import { fetchItemsActionType, ProductType, ActionsTypes } from '../../types';
+import { fetchItemsActionType, ProductType, ActionsTypes, sortItemsActionType } from '../../types';
+import { ascendingSort, descendingSort } from '../../utils';
 
 export type stateType = {
-  items: ProductType[]
+  items: ProductType[],
+  isLoaded: boolean,
 }
 
 const initialState: stateType = {
-  items: []
+  items: [],
+  isLoaded: false
 }
 
-const itemsReducer = (state = initialState, action: fetchItemsActionType ): stateType => {
+const itemsReducer = (state = initialState, action: fetchItemsActionType | sortItemsActionType ): stateType => {
   switch (action.type) {
     case ActionsTypes.FETCH_PRODUCTS:
       return {
         ...state,
+        isLoaded: true,
         items: action.payload,
       }
+    case ActionsTypes.SORT_ASC:
+      return {
+        ...state,
+        items: [...ascendingSort(state.items)],
+      }
+      case ActionsTypes.SORT_DESC:
+        return {
+          ...state,
+          items: [...descendingSort(state.items, 'priceNew')],
+        }
+      case ActionsTypes.SORT_POP:
+        return {
+          ...state,
+          items: [...descendingSort(state.items, 'rating')],
+        }
+  
     default:
       return state;
   };
