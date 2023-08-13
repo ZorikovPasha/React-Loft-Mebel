5b05cc03d16a0603ed6a7b42a3c39aa54cb30fbf
Justin11Delano <zorikov.pasha@mail.ru>
2023-08-13 15:42:11 +0300

    fix: fixing sort popup

--- client/src/components/Catalog/SortPopup.tsx
+++ client/src/components/Catalog/SortPopup.tsx
@@ -4,75 +4,70 @@ interface ISortPopupProps {
   onSelectSortType: (cat: string) => void
 }
 
-export const SortPopup: React.FC<ISortPopupProps> = () => {
-  // const [isSortPopupVisible, toggleSortPopupVisibility] = React.useState(false)
-  // const [activeCat, setActiveCat] = React.useState<string>('asc')
+export const SortPopup: React.FC<ISortPopupProps> = ({ onSelectSortType }) => {
+  const items = [
+    {
+      value: 'desc',
+      text: 'по убыванию цены'
+    },
+    {
+      value: 'asc',
+      text: 'по возрастанию цены'
+    },
+    {
+      value: 'pop',
+      text: 'по популярности'
+    }
+  ]
 
-  // const popupRef = React.useRef(null)
+  const [isSortPopupVisible, toggleSortPopupVisibility] = React.useState(false)
+  const [activeCat, setActiveCat] = React.useState<string>('asc')
 
-  // const onSortBtnClick = (): void => {
-  //   toggleSortPopupVisibility(!isSortPopupVisible)
-  // }
+  const popupRef = React.useRef(null)
 
-  // const onListItemClick = (
-  // e: React.MouseEvent<HTMLLIElement>
-  // ): void => {
-  // const target = e.target as HTMLLIElement
-  // setActiveCat(target.attributes.value.textContent)
-  // toggleSortPopupVisibility(false)
-  // onSelectSortType(target.attributes.value.textContent)
-  // }
+  const onSortBtnClick = (): void => {
+    toggleSortPopupVisibility(!isSortPopupVisible)
+  }
 
-  // React.useEffect(() => {
-  //   const handleOutsidePopupClick = (e: MouseEvent): void => {
-  //     const path = e.path || (e.composedPath && e.composedPath())
-  //     toggleSortPopupVisibility((prev) => (prev && !path.includes(popupRef.current) ? false : prev))
-  //   }
+  const onListItemClick = (type: string) => () => {
+    setActiveCat(type)
+    toggleSortPopupVisibility(false)
+    onSelectSortType(type)
+  }
 
-  //   document.body.addEventListener('click', handleOutsidePopupClick)
+  React.useEffect(() => {
+    const handleOutsidePopupClick = (e: MouseEvent): void => {
+      const path = e.path || (e.composedPath && e.composedPath())
+      toggleSortPopupVisibility((prev) => (prev && !path.includes(popupRef.current) ? false : prev))
+    }
 
-  //   return () => {
-  //     document.body.removeEventListener('click', handleOutsidePopupClick)
-  //   }
-  // }, [])
+    document.body.addEventListener('click', handleOutsidePopupClick)
 
-  // const items = [
-  //   {
-  //     value: 'desc',
-  //     text: 'по убыванию цены'
-  //   },
-  //   {
-  //     value: 'asc',
-  //     text: 'по возрастанию цены'
-  //   },
-  //   {
-  //     value: 'pop',
-  //     text: 'по популярности'
-  //   }
-  // ]
+    return () => {
+      document.body.removeEventListener('click', handleOutsidePopupClick)
+    }
+  }, [])
 
   return (
-    <></>
-    // <button
-    //   className='controls__sort'
-    //   onClick={onSortBtnClick}
-    //   ref={popupRef}
-    // >
-    //   Сортировать: <span className='controls__sort-choice'>{items.find((item) => item.value === activeCat)?.text}</span>
-    //   {isSortPopupVisible && (
-    //     <ul className='sort-list'>
-    //       {items.map(({ value, text }) => (
-    //         <li
-    //           className={`sort-list__item ${activeCat === value ? 'active' : ''}`}
-    //           onClick={onListItemClick}
-    //           value={value}
-    //           key={value}
-    //         >
-    //           {text}
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   )}
-    // </button>
+    <button
+      className='controls__sort'
+      onClick={onSortBtnClick}
+      ref={popupRef}
+    >
+      Сортировать: <span className='controls__sort-choice'>{items.find((item) => item.value === activeCat)?.text}</span>
+      {isSortPopupVisible && (
+        <ul className='sort-list'>
+          {items.map(({ value, text }) => (
+            <li
+              className={`sort-list__item ${activeCat === value ? 'active' : ''}`}
+              onClick={onListItemClick(value)}
+              key={value}
+            >
+              {text}
+            </li>
+          ))}
+        </ul>
+      )}
+    </button>
   )
 }
