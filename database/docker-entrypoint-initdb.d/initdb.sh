c0afa0742b04f46329898ba39749316134809b30
Justin11Delano <86666268+ZorikovPasha@users.noreply.github.com>
2023-09-01 17:45:15 +0300

    feat: #33 editing ci/cd

--- database/docker-entrypoint-initdb.d/initdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-db=loft_furniture
-username=postgres
-password=postgres
-
-echo "Creating user '$username' and database '$db'"
-psql -U $POSTGRES_USER -c "create user $username with LOGIN CREATEROLE CREATEDB password '$password';"
-psql -U $POSTGRES_USER -c "create database $db;"
